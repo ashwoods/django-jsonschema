e74f5e9bdb292de269768044575a4e776163940a
Jonas Svensson <jonas@travelbird.nl>
2013-09-04 16:17:44 +0200

    Added support for Django 1.3. Implemeted schema like http://www.alpacajs.org/

--- djangojsonschema/jsonschema.py
+++ djangojsonschema/jsonschema.py
@@ -1,4 +1,5 @@
-#TODO find a better submodule name
+import inspect
+
 from django.forms import widgets, fields
 
 
@@ -8,33 +9,66 @@ def pretty_name(name):
         return u''
     return name.replace('_', ' ').capitalize()
 
+
 class DjangoFormToJSONSchema(object):
-    def convert_form(self, form, json_schema=None):
-        if json_schema is None:
-            json_schema = {
-                #'title':dockit_schema._meta
-                #'description'
-                'type':'object',
-                'properties':{}, #TODO SortedDict
-            }
-        #CONSIDER: base_fields when given a class, fields for when given an instance
-        for name, field in form.base_fields.iteritems():
-            json_schema['properties'][name] = self.convert_formfield(name, field, json_schema)
-        return json_schema
-    
+
     input_type_map = {
         'text': 'string',
     }
-    
+
+    def get_form_fields(self, form):
+        """ base_fields when given a class,
+            fields for when given an instance
+        """
+        is_class = inspect.isclass(form)
+        if is_class:
+            form_fields = form.base_fields.iteritems()
+        else:
+            form_fields = form.fields.iteritems()
+        return form_fields
+
+    def populate_schema_defaults_from_instance(self, json_schema, instance):
+        """ Populate the json_schema with default
+            values if an instance is given """
+        for name, properties in json_schema.get('properties').items():
+            if hasattr(instance, name):
+                value = getattr(instance, name)
+                json_schema['properties'][name]['default'] = unicode(value)
+        return json_schema
+
+    def convert_form(self, form, json_schema=None, instance=None):
+        if json_schema is None:
+
+            json_schema = {
+                'title': pretty_name(form.__class__.__name__),
+                'description': form.__doc__ or '',
+                'type': 'object',
+                'properties': {},
+            }
+
+        form_fields = self.get_form_fields(form)
+
+        for name, field in form_fields:
+            field_data = self.convert_formfield(name, field, json_schema)
+            json_schema['properties'][name] = field_data
+
+        if instance:
+            """ Populate the default values if an instance is given """
+            self.populate_schema_defaults_from_instance(
+                json_schema, instance)
+
+        return json_schema
+
     def convert_formfield(self, name, field, json_schema):
         #TODO detect bound field
         widget = field.widget
         target_def = {
-            'title': field.label or pretty_name(name),
+            'title': pretty_name(name),
             'description': field.help_text,
-        }
-        if field.required:
-            target_def['required'] = [name] #TODO this likely is not correct
+            'readonly': widget.attrs.get('readonly', False),
+            'required': field.required,
+            'default': field.initial or ''}
+
         #TODO JSONSchemaField; include subschema and ref the type
         if isinstance(field, fields.URLField):
             target_def['type'] = 'string'
@@ -60,43 +94,15 @@ class DjangoFormToJSONSchema(object):
         elif isinstance(widget, widgets.CheckboxInput):
             target_def['type'] = 'boolean'
         elif isinstance(widget, widgets.Select):
-            if widget.allow_multiple_selected:
-                target_def['type'] = 'array'
-            else:
-                target_def['type'] = 'string'
+            target_def['type'] = 'string'
+            if hasattr(widget, 'allow_multiple_selected'):
+                if widget.allow_multiple_selected:
+                    target_def['type'] = 'array'
             target_def['enum'] = [choice[0] for choice in field.choices]
         elif isinstance(widget, widgets.Input):
-            translated_type = self.input_type_map.get(widget.input_type, 'string')
+            translated_type = self.input_type_map.get(
+                widget.input_type, 'string')
             target_def['type'] = translated_type
         else:
             target_def['type'] = 'string'
         return target_def
-        
-class DjangoModelToJSONSchema(DjangoFormToJSONSchema):
-    def convert_model(self, model, json_schema=None):
-        model_form = None #TODO convert to model form
-        #TODO handle many2many and inlines
-        return self.convert_form(model_form, json_schema)
-
-#TODO move to django-dockit
-class DocKitSchemaToJSONSchema(DjangoFormToJSONSchema):
-    def convert_dockitschema(self, dockit_schema, json_schema=None):
-        if json_schema is None:
-            json_schema = {
-                #'title':dockit_schema._meta
-                #'description'
-                'type':'object',
-                'properties':{}, #TODO SortedDict
-            }
-        for key, field in dockit_schema._meta.fields.iteritems():
-            json_schema['properties'][key] = self.convert_dockitfield(key, field, json_schema)
-        return json_schema
-    
-    def convert_dockitfield(self, name, field, json_schema):
-        #if simple field, get the form field
-        if True: #TODO is simple
-            formfield = field.formfield()
-            return self.convert_formfield(formfield, json_schema)
-        #else: #complex stuff
-        target_def = {}
-
